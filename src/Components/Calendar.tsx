49e0936c56f51fc3b494015091fa782312baa923
Francisco Salanova <fs185092@ncr.com>
2024-10-15 19:16:18 -0300

    Adding calendar days

--- src/Components/Calendar.tsx
+++ src/Components/Calendar.tsx
@@ -1,8 +1,34 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { formatDate } from "../utils/formatDate"
+import {
+  addMonths,
+  eachDayOfInterval,
+  endOfDay,
+  endOfMonth,
+  endOfWeek,
+  isBefore,
+  isSameMonth,
+  startOfMonth,
+  startOfWeek,
+  subMonths,
+} from "date-fns"
+import { cc } from "../utils/cc"
+
+type CalendarDayProps = {
+  day: Date
+  showWeekName: boolean
+  selectedMonth: Date
+}
 
 export const Calendar = () => {
   const [selectedMonth, setSelectedMonth] = useState(new Date())
+
+  const calendarDays = useMemo(() => {
+    const firstWeekStart = startOfWeek(startOfMonth(selectedMonth))
+    const lastWeekEnd = endOfWeek(endOfMonth(selectedMonth))
+
+    return eachDayOfInterval({ start: firstWeekStart, end: lastWeekEnd })
+  }, [selectedMonth])
 
   return (
     <div className="calendar">
@@ -11,14 +37,83 @@ export const Calendar = () => {
           Today
         </button>
         <div>
-          <button className="month-change-btn">&lt;</button>
-          <button className="month-change-btn">&gt;</button>
+          <button
+            className="month-change-btn"
+            onClick={() => setSelectedMonth((m) => subMonths(m, 1))}
+          >
+            &lt;
+          </button>
+          <button
+            className="month-change-btn"
+            onClick={() => setSelectedMonth((m) => addMonths(m, 1))}
+          >
+            &gt;
+          </button>
         </div>
         <span className="month-title">
           {formatDate(selectedMonth, { month: "long", year: "numeric" })}
         </span>
       </div>
-      <div className="days"></div>
+      <div className="days">
+        {calendarDays.map((day, index) => {
+          return (
+            <CalendarDay
+              key={day.getTime()}
+              day={day}
+              showWeekName={index < 7}
+              selectedMonth={selectedMonth}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+const CalendarDay = ({
+  day,
+  showWeekName,
+  selectedMonth,
+}: CalendarDayProps) => {
+  const [isNewEventModalOpen, setIsNewEventModalOpen] = useState(false)
+  const [isViewMoreEventsModalOpen, setIsViewMoreEventsModalOpen] =
+    useState(false)
+
+  return (
+    <div
+      className={cc(
+        "day",
+        !isSameMonth(day, selectedMonth) && "non-month-day",
+        isBefore(endOfDay(day), new Date()) && "old-month-day"
+      )}
+    >
+      <div className="day-header">
+        {showWeekName && (
+          <div className="week-name">
+            {formatDate(day, { weekday: "short" })}
+          </div>
+        )}
+        <div className="day-number">{formatDate(day, { day: "numeric" })}</div>
+        <button className="add-event-btn">+</button>
+      </div>
+    </div>
+  )
+}
+
+const CalendarEvent = () => {
+  return (
+    <div className="events">
+      <button className="all-day-event blue event">
+        <div className="event-name">Short</div>
+      </button>
+      <button className="all-day-event green event">
+        <div className="event-name">Long Event Name That Just Keeps Going</div>
+      </button>
+      <button className="event">
+        <div className="color-dot blue"></div>
+        <div className="event-time">7am</div>
+        <div className="event-name">Event Name</div>
+      </button>
     </div>
   )
 }
--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,3 +1,6 @@
-export const formatDate = (date, options) => {
+export const formatDate = (
+  date: Date,
+  options?: Intl.DateTimeFormatOptions
+) => {
   return new Intl.DateTimeFormat(undefined, options).format(date)
 }
